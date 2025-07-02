d4a2d54fe2f5d8558aedbae61b3aa5d29e8752f3
AbdulboriyIsroilov <abdulboriisroilov4515@gmail.com>
2025-07-02 14:16:10 +0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	lib/main.dart

---
+++ lib/home_scaffold.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class home_scaffold extends StatelessWidget {
+  const home_scaffold({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text(
+          "Hello World",
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.w500,
+            color: Colors.white70,
+          ),
+        ),
+        centerTitle: true,
+        leading: Icon(Icons.arrow_back),
+        actionsPadding: EdgeInsets.only(right: 20),
+        actions: [
+          Icon(Icons.add_a_photo_outlined),
+          Icon(Icons.account_balance_wallet_outlined),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          "New Proyekt",
+          style: TextStyle(
+            fontSize: 30,
+            color: Colors.indigo,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:new_proyekt/home_scaffold.dart';
 
 void main() {
   runApp(MyFlutter());
@@ -12,36 +13,7 @@ class MyFlutter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: Text(
-            "Hello World",
-            style: TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.w500,
-              color: Colors.white70,
-            ),
-          ),
-          centerTitle: true,
-          leading: Icon(Icons.arrow_back),
-          actionsPadding: EdgeInsets.only(right: 20),
-          actions: [
-            Icon(Icons.add_a_photo_outlined),
-            Icon(Icons.account_balance_wallet_outlined),
-          ],
-        ),
-        body: Center(
-          child: Text(
-            "New Proyekt",
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.indigo,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-        )
-      ),
+      home: home_scaffold(),
     );
   }
 }
