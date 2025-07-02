397c3b31a67c3e8ae6517e55888c39fb15066670
AbdulboriyIsroilov <abdulboriisroilov4515@gmail.com>
2025-07-02 13:39:40 +0500

    shunchaki yozgan kodimni gitga yukladim

---
+++ android/app/src/main/kotlin/com/example/new_proyekt/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.new_proyekt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyFlutter());
+}
+
+class MyFlutter extends StatelessWidget {
+  MyFlutter({super.key,});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: text,
+        ),
+      ),
+    );
+  }
+}
